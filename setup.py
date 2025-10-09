2c3be886eef31046455b2cec3a9c84644a597977
mkielbus <mateusz.kielbus.mk@gmail.com>
2025-10-09 17:37:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements():
 
 setup(
     name="DeOldify",
-    version="0.0.1",
+    version="1.0.0",
     packages=find_packages(exclude=["tests"]),
     url="https://github.com/jantic/DeOldify",
     license="MIT License",
@@ -25,16 +25,10 @@ setup(
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Framework :: Jupyter",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
-        # "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     install_requires=get_requirements(),
-    python_requires=">=3.6",
 )
